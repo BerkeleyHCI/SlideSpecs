99c803425f044416c4ebe5cf5b49bdd4aaf1b4c1
Jeremy Warner <jeremy.warner@autodesk.com>
2018-07-25 20:31:03 -0700

    adding meteor ui elements for uploading file

--- app/imports/api/files/files.js
+++ app/imports/api/files/files.js
@@ -1,14 +1,8 @@
-import {Mongo} from 'meteor/mongo';
-import {SimpleSchema} from 'meteor/aldeed:simple-schema';
-import {Factory} from 'meteor/factory';
-import {Todos} from '../todos/todos.js';
 import {Meteor} from 'meteor/meteor';
 import {FilesCollection} from 'meteor/ostrio:files';
 
-// New code
-
 const Files = new FilesCollection({
-  collectionName: 'Images',
+  collectionName: 'Files',
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload(file) {
     // Allow upload files under 10MB, and only in png/jpg/jpeg formats
@@ -18,12 +12,5 @@ const Files = new FilesCollection({
     return 'Please upload image, with size equal or less than 10MB';
   },
 });
-
-/*
-  Meteor.subscribe('files.images.all');
-  Meteor.publish('files.images.all', function() {
-    return Images.find().cursor;
-  });
- * */
 
 export default Files;
