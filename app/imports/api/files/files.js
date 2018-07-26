794a80ee1af951022d9fe38e858adda2bda1572a
Jeremy Warner <jeremy.warner@autodesk.com>
2018-07-25 18:20:03 -0700

    schema

--- app/imports/api/files/files.js
+++ app/imports/api/files/files.js
@@ -2,66 +2,12 @@ import {Mongo} from 'meteor/mongo';
 import {SimpleSchema} from 'meteor/aldeed:simple-schema';
 import {Factory} from 'meteor/factory';
 import {Todos} from '../todos/todos.js';
-
 import {Meteor} from 'meteor/meteor';
 import {FilesCollection} from 'meteor/ostrio:files';
 
-// Deny all client-side updates since we will be using methods to manage this collection
-Files.deny({
-  insert() {
-    return true;
-  },
-  update() {
-    return true;
-  },
-  remove() {
-    return true;
-  },
-});
-
-Files.schema = new SimpleSchema({
-  name: {type: String},
-  incompleteCount: {type: Number, defaultValue: 0},
-  userId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-});
-
-Files.attachSchema(Files.schema);
-
-// This represents the keys from Files objects that should be published
-// to the client. If we add secret properties to List objects, don't list
-// them here to keep them private to the server.
-Files.publicFields = {
-  name: 1,
-  incompleteCount: 1,
-  userId: 1,
-};
-
-Factory.define('list', Files, {});
-
-Files.helpers({
-  // A list is considered to be private if it has a userId set
-  isPrivate() {
-    return !!this.userId;
-  },
-  isLastPublicList() {
-    const publicListCount = Files.find({userId: {$exists: false}}).count();
-    return !this.isPrivate() && publicListCount === 1;
-  },
-  editableBy(userId) {
-    if (!this.userId) {
-      return true;
-    }
-
-    return this.userId === userId;
-  },
-  todos() {
-    return Todos.find({listId: this._id}, {sort: {createdAt: -1}});
-  },
-});
-
 // New code
 
-const Images = new FilesCollection({
+const Files = new FilesCollection({
   collectionName: 'Images',
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload(file) {
@@ -83,4 +29,12 @@ if (Meteor.isServer) {
   });
 }
 
-export const Files = new FilesCollection('Files');
+Files.schema = new SimpleSchema({
+  name: {type: String},
+  incompleteCount: {type: Number, defaultValue: 0},
+  userId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
+});
+
+Files.attachSchema(Files.schema);
+
+export default Files;
