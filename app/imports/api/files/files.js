4b1defe8673c459bbfd4af8ee1d9fded25169201
Jeremy Warner <jeremy.warner@autodesk.com>
2018-07-25 17:38:43 -0700

    uploading files

--- app/imports/api/files/files.js
+++ app/imports/api/files/files.js
@@ -3,6 +3,9 @@ import {SimpleSchema} from 'meteor/aldeed:simple-schema';
 import {Factory} from 'meteor/factory';
 import {Todos} from '../todos/todos.js';
 
+import {Meteor} from 'meteor/meteor';
+import {FilesCollection} from 'meteor/ostrio:files';
+
 class FilesCollection extends Mongo.Collection {
   insert(list, callback, locale = 'en') {
     const ourList = list;
@@ -80,3 +83,27 @@ Files.helpers({
     return Todos.find({listId: this._id}, {sort: {createdAt: -1}});
   },
 });
+
+// New code
+
+const Images = new FilesCollection({
+  collectionName: 'Images',
+  allowClientCode: false, // Disallow remove files from Client
+  onBeforeUpload(file) {
+    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+      return true;
+    }
+    return 'Please upload image, with size equal or less than 10MB';
+  },
+});
+
+if (Meteor.isClient) {
+  Meteor.subscribe('files.images.all');
+}
+
+if (Meteor.isServer) {
+  Meteor.publish('files.images.all', function() {
+    return Images.find().cursor;
+  });
+}
---
+++ app/imports/ui/components/FileUpload.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BaseComponent from './BaseComponent.jsx';
+
+class FileUpload extends BaseComponent {
+  render() {
+    return <h1>FILE UPLOAD</h1>;
+  }
+}
+
+export default FileUpload;
