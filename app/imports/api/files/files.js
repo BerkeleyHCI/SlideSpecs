fab94f2e01dfc5bb4ffe43ef2fb259eacf33e423
Jeremy Warner <jeremy.warner@autodesk.com>
2018-07-27 12:25:49 -0700

    mirror lists schema for now

--- app/imports/api/files/files.js
+++ app/imports/api/files/files.js
@@ -1,16 +1,15 @@
 import {Meteor} from 'meteor/meteor';
 import {FilesCollection} from 'meteor/ostrio:files';
 
-const Files = new FilesCollection({
+export const Files = new FilesCollection({
   collectionName: 'Files',
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload(file) {
     // Allow upload files under 10MB, and only in png/jpg/jpeg formats
     if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
       return true;
+    } else {
+      return 'Please upload image, with size equal or less than 10MB';
     }
-    return 'Please upload image, with size equal or less than 10MB';
   },
 });
-
-export default Files;
--- app/imports/api/lists/lists.js
+++ app/imports/api/lists/lists.js
@@ -1,17 +1,17 @@
-import { Mongo } from 'meteor/mongo';
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-import { Factory } from 'meteor/factory';
-import { Todos } from '../todos/todos.js';
+import {Mongo} from 'meteor/mongo';
+import {SimpleSchema} from 'meteor/aldeed:simple-schema';
+import {Factory} from 'meteor/factory';
+import {Todos} from '../todos/todos.js';
 
 class ListsCollection extends Mongo.Collection {
   insert(list, callback, locale = 'en') {
     const ourList = list;
     if (!ourList.name) {
-      const defaultName = 'insert list'
+      const defaultName = 'insert list';
       let nextLetter = 'A';
       ourList.name = `${defaultName} ${nextLetter}`;
 
-      while (this.findOne({ name: ourList.name })) {
+      while (this.findOne({name: ourList.name})) {
         // not going to be too smart here, can go past Z
         nextLetter = String.fromCharCode(nextLetter.charCodeAt(0) + 1);
         ourList.name = `${defaultName} ${nextLetter}`;
@@ -21,7 +21,7 @@ class ListsCollection extends Mongo.Collection {
     return super.insert(ourList, callback);
   }
   remove(selector, callback) {
-    Todos.remove({ listId: selector });
+    Todos.remove({listId: selector});
     return super.remove(selector, callback);
   }
 }
@@ -30,15 +30,21 @@ export const Lists = new ListsCollection('Lists');
 
 // Deny all client-side updates since we will be using methods to manage this collection
 Lists.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; },
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  },
 });
 
 Lists.schema = new SimpleSchema({
-  name: { type: String },
-  incompleteCount: { type: Number, defaultValue: 0 },
-  userId: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true },
+  name: {type: String},
+  incompleteCount: {type: Number, defaultValue: 0},
+  userId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
 });
 
 Lists.attachSchema(Lists.schema);
@@ -60,7 +66,7 @@ Lists.helpers({
     return !!this.userId;
   },
   isLastPublicList() {
-    const publicListCount = Lists.find({ userId: { $exists: false } }).count();
+    const publicListCount = Lists.find({userId: {$exists: false}}).count();
     return !this.isPrivate() && publicListCount === 1;
   },
   editableBy(userId) {
@@ -71,6 +77,6 @@ Lists.helpers({
     return this.userId === userId;
   },
   todos() {
-    return Todos.find({ listId: this._id }, { sort: { createdAt: -1 } });
+    return Todos.find({listId: this._id}, {sort: {createdAt: -1}});
   },
 });
