28c04d9084f57e7152bb0079b0a5fd103b3f5241
Jeremy Warner <jeremy.warner@autodesk.com>
2018-08-07 08:33:17 -0700

    register service api

--- app/imports/startup/server/register-api.js
+++ app/imports/startup/server/register-api.js
@@ -2,3 +2,5 @@ import '../../api/files/methods.js';
 import '../../api/files/server/publications.js';
 import '../../api/sessions/methods.js';
 import '../../api/sessions/server/publications.js';
+import '../../api/comments/methods.js';
+import '../../api/comments/server/publications.js';
