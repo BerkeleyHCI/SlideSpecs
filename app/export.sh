6acd3d07f8f98305c7165cda153866718bb51e29
Jeremy Warner <jeremywrnr@gmail.com>
2018-09-13 17:44:33 -0700

    adding users and files

--- app/export.sh
+++ app/export.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 data=../data
-declare -a arr=("Sessions" "Files" "Comments" "Events")
+declare -a arr=("users" "Sessions" "files" "Comments" "Events")
 mkdir -p $data
 for i in "${arr[@]}"; do
     mongoexport --port 3001 --db meteor --collection "$i" --out "$data/$i.json"
