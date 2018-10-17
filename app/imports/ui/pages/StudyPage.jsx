c94036e4b81768e4abc7ee459d62e6813e98a38d
Jeremy Warner <jeremywrnr@gmail.com>
2018-10-17 22:53:53 +0200

    updating the study page

--- app/imports/ui/pages/StudyPage.jsx
+++ app/imports/ui/pages/StudyPage.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 // LINK STUDY PAGE FOR ARVR CLASS 9/12
+// note: updated for class on 10/17
 
 const StudyPageLink = (link, i) => {
   return (
@@ -14,20 +15,42 @@ const StudyPageLink = (link, i) => {
 
 class StudyPage extends Component {
   render() {
+    // for 9/12
+    //const linkIds = [
+    //'sekvKeDoovKogBpJE',
+    //'DBqSqEwmqjhomvP24',
+    //'wZvSA8jh93WW2Ytqn',
+    //'ccE6XkL2iRRbj9dkK',
+    //'NBxpCTWSK4ik2kekM',
+    //'KnsJK63N3f9vneyqy',
+    //'m4h8GN8HyvAFsLBWm',
+    //'4sHvcTKNR8H49Qxoe',
+    //'SL2u4b47BsJK5C6rg',
+    //'d9ePEZwoA4vvioqR2',
+    //'LWTejnPdPjv6G7dub',
+    //'tkK5wHfomfjR79yJk',
+    //'c2aYLAoHzfTiXmWRX',
+    //];
+
+    // for 10/17
     const linkIds = [
-      'sekvKeDoovKogBpJE',
-      'DBqSqEwmqjhomvP24',
-      'wZvSA8jh93WW2Ytqn',
-      'ccE6XkL2iRRbj9dkK',
-      'NBxpCTWSK4ik2kekM',
-      'KnsJK63N3f9vneyqy',
-      'm4h8GN8HyvAFsLBWm',
-      '4sHvcTKNR8H49Qxoe',
-      'SL2u4b47BsJK5C6rg',
-      'd9ePEZwoA4vvioqR2',
-      'LWTejnPdPjv6G7dub',
-      'tkK5wHfomfjR79yJk',
-      'c2aYLAoHzfTiXmWRX',
+      'rc9hj8ccnFMR7Xrrc', // group 1
+      'ormbW5fLWdwb9uJhn',
+      'NQyqNmFjjLnT7G4K6',
+      'wkosB2emHrdLtbyf8',
+      '8See5hFhZrk5m3E8M',
+      'R2Kf5cMQZkKQ7eupZ',
+      'ty8FMobbtxR6sEnKz',
+      'u72H322FYyp5uT4Ez',
+      '6BdoZffPNbpWrToXA',
+      '4CPNqDYbzuCsuDZiT',
+      'bqGqonWpF5LNSbSHC',
+      'hbGYCjxKMLTE3cgSk',
+      'dXYn8TdRpx9go4d5p',
+      'tTNWCMxQ55qe5acpx',
+      '4gHRkSXo4kEA87FGK',
+      'QNyRENSd4PJncxLSb',
+      'AvcopdT6TLdh4Jz5E', // group 17
     ];
 
     return (
