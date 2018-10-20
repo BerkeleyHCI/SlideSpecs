6dfd34f1c19f001287efabe2cf60f8012ff119dc
Jeremy Warner <jeremywrnr@gmail.com>
2018-10-20 19:46:35 +0200

    dont put the entire database in the clients browser LOL

--- app/imports/api/comments/server/publications.js
+++ app/imports/api/comments/server/publications.js
@@ -1,4 +1,11 @@
 import {Meteor} from 'meteor/meteor';
 import {Comments} from '../comments.js';
 
-Meteor.publish('comments', session => Comments.find({session: session}));
+Meteor.publish('comments', session => {
+  check(session, String);
+  if (!session) {
+    return this.ready();
+  } else {
+    return Comments.find({session: session});
+  }
+});
--- app/imports/api/sessions/server/publications.js
+++ app/imports/api/sessions/server/publications.js
@@ -3,14 +3,4 @@
 import {Meteor} from 'meteor/meteor';
 import {Sessions} from '../sessions.js';
 
-/*
-Meteor.publish('sessions', function sessionsPrivate() {
-  if (!this.userId) {
-    return this.ready();
-  } else {
-    return Sessions.find({userId: this.userId});
-  }
-});
-**/
-
 Meteor.publish('sessions', () => Sessions.find({}));
