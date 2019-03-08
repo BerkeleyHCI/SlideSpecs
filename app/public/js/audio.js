fa55badd3274335cfc59f0b045bbe8997788d846
Jeremy Warner <jeremywrnr@gmail.com>
2019-03-08 12:34:42 -0800

    fix recording paths

---
+++ app/public/js/audio.js
@@ -0,0 +1,151 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+let audioContext = new AudioContext();
+let audioInput = null,
+    realAudioInput = null,
+    inputPoint = null,
+    audioRecorder = null;
+let rafID = null;
+let analyserContext = null;
+let canvasWidth, canvasHeight;
+let recIndex = 0;
+
+function saveAudio() {
+    audioRecorder.exportWAV(doneEncoding);
+}
+
+function gotBuffers(buffers) {
+    console.log(buffers)
+    let canvas = document.getElementById("wavedisplay");
+
+    drawBuffer(
+        canvas.width,
+        canvas.height,
+        canvas.getContext("2d"),
+        buffers[0]
+    );
+
+    // the ONLY time gotBuffers is called is right after a new recording is completed -
+    // so here's where we should set up the download.
+    audioRecorder.exportWAV(doneEncoding);
+}
+
+function doneEncoding(blob) {
+    Recorder.setupDownload(
+        blob,
+        "myRecording" + (recIndex < 10 ? "0" : "") + recIndex + ".wav"
+    );
+    recIndex++;
+}
+
+function clearRecording() {
+    audioRecorder.clear();
+}
+
+function toggleRecording(recording) {
+    if (recording) {
+        // stop recording
+        audioRecorder.stop();
+        audioRecorder.getBuffers(gotBuffers);
+    } else {
+        // start recording
+        if (!audioRecorder) return;
+        audioRecorder.record();
+    }
+}
+
+function convertToMono(input) {
+    let splitter = audioContext.createChannelSplitter(2);
+    let merger = audioContext.createChannelMerger(2);
+
+    input.connect(splitter);
+    splitter.connect(merger, 0, 0);
+    splitter.connect(merger, 0, 1);
+    return merger;
+}
+
+function cancelAnalyserUpdates() {
+    window.cancelAnimationFrame(rafID);
+    rafID = null;
+}
+
+function updateAnalysers(time) {
+    if (!analyserContext) {
+        let canvas = document.getElementById("analyser");
+        canvasWidth = canvas.width;
+        canvasHeight = canvas.height;
+        analyserContext = canvas.getContext("2d");
+    }
+
+    // analyzer draw code here
+    {
+        let SPACING = 3;
+        let BAR_WIDTH = 1;
+        let numBars = Math.round(canvasWidth / SPACING);
+        let freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
+
+        analyserNode.getByteFrequencyData(freqByteData);
+
+        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        analyserContext.fillStyle = "#F6D565";
+        analyserContext.lineCap = "round";
+        let multiplier = analyserNode.frequencyBinCount / numBars;
+
+        // Draw rectangle for each frequency bin.
+        for (let i = 0; i < numBars; ++i) {
+            let magnitude = 0;
+            let offset = Math.floor(i * multiplier);
+            // gotta sum/average the block, or we miss narrow-bandwidth spikes
+            for (let j = 0; j < multiplier; j++)
+                magnitude += freqByteData[offset + j];
+            magnitude = magnitude / multiplier;
+            let magnitude2 = freqByteData[i * multiplier];
+            analyserContext.fillStyle =
+                "rgb( " + Math.round((i * 360) / numBars) + ", 100%, 50%)";
+            analyserContext.fillRect(
+                i * SPACING,
+                canvasHeight,
+                BAR_WIDTH,
+                -magnitude
+            );
+        }
+    }
+
+    rafID = window.requestAnimationFrame(updateAnalysers);
+}
+
+function toggleMono() {
+    if (audioInput != realAudioInput) {
+        audioInput.disconnect();
+        realAudioInput.disconnect();
+        audioInput = realAudioInput;
+    } else {
+        realAudioInput.disconnect();
+        audioInput = convertToMono(realAudioInput);
+    }
+
+    audioInput.connect(inputPoint);
+}
+
+function gotStream(stream) {
+    inputPoint = audioContext.createGain();
+
+    // Create an AudioNode from the stream.
+    realAudioInput = audioContext.createMediaStreamSource(stream);
+    audioInput = realAudioInput;
+    audioInput.connect(inputPoint);
+
+    //    audioInput = convertToMono( input );
+
+    analyserNode = audioContext.createAnalyser();
+    analyserNode.fftSize = 2048;
+    inputPoint.connect(analyserNode);
+
+    audioRecorder = new Recorder(inputPoint);
+
+    zeroGain = audioContext.createGain();
+    zeroGain.gain.value = 0.0;
+    inputPoint.connect(zeroGain);
+    zeroGain.connect(audioContext.destination);
+    updateAnalysers();
+}
