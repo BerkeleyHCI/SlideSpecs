f97e48648c367cfd6482bbb631aeb218e41d5f1e
Jeremy Warner <jeremywrnr@gmail.com>
2019-03-14 11:54:19 -0700

    turning up the gain on the mic

--- app/imports/ui/components/AlertLink.jsx
+++ app/imports/ui/components/AlertLink.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Meteor } from "meteor/meteor";
-import { Session } from "meteor/session.js";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Meteor} from 'meteor/meteor';
+import {Session} from 'meteor/session.js';
 
 class AlertLink extends Component {
   render() {
-    const { link, text, bText, blank, center } = this.props;
+    const {link, text, bText, blank, center} = this.props;
 
     let ref;
     if (blank) {
       ref = {
-        target: "_blank",
-        rel: "noopener noreferrer"
+        target: '_blank',
+        rel: 'noopener noreferrer',
       };
     }
 
     return (
       <a className="link-alert" href={link} {...ref}>
-        <div className={"alert " + (center ? "centered" : "")}>
+        <div className={'alert ' + (center ? 'centered' : '')}>
           {text}
           {bText && (
             <button className="pull-right btn-menu btn-primary">{bText}</button>
@@ -32,14 +32,14 @@ AlertLink.propTypes = {
   link: PropTypes.string,
   text: PropTypes.string,
   bText: PropTypes.string,
-  blank: PropTypes.bool
+  blank: PropTypes.bool,
 };
 
 AlertLink.defaultProps = {
-  link: "/",
-  text: "link",
+  link: '/',
+  text: 'link',
   center: false,
-  blank: false
+  blank: false,
 };
 
 export default AlertLink;
