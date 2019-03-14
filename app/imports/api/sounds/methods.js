6a18eb89e134d79d8dd1e8704c1d69a0c16ca284
Jeremy Warner <jeremywrnr@gmail.com>
2019-03-13 19:34:49 -0700

    rename server log

--- app/imports/api/sounds/methods.js
+++ app/imports/api/sounds/methods.js
@@ -1,61 +1,63 @@
-import { ValidatedMethod } from "meteor/mdg:validated-method";
-import { SimpleSchema } from "meteor/aldeed:simple-schema";
-import { Sounds } from "./sounds.js";
+import {ValidatedMethod} from 'meteor/mdg:validated-method';
+import {SimpleSchema} from 'meteor/aldeed:simple-schema';
+import {Sounds} from './sounds.js';
 
 export const renameSound = new ValidatedMethod({
-  name: "sounds.rename",
+  name: 'sounds.rename',
   validate: new SimpleSchema({
-    soundId: { type: String },
-    newName: { type: String }
+    soundId: {type: String},
+    newName: {type: String},
   }).validator(),
-  run({ soundId, newName }) {
-    this.unblock(); // <-- Use to make this method asynchronous
-    const good = Sounds.collection.update(soundId, { $set: { name: newName } });
+  run({soundId, newName}) {
+    this.unblock();
+    const good = Sounds.collection.update(soundId, {$set: {name: newName}});
     return good;
-  }
+  },
 });
 
 export const updateSound = new ValidatedMethod({
-  name: "sounds.update",
+  name: 'sounds.update',
   validate: new SimpleSchema({
-    soundId: { type: String },
+    soundId: {type: String},
     transcript: {type: String},
-    results: { type: String },
+    results: {type: String},
   }).validator(),
-  run({ soundId, transcript, results,  }) {
-      this.unblock(); // <-- Use to make this method asynchronous
-      Sounds.collection.update(soundId, { $set: {
-        "meta.transcript": transcript,
-        "meta.results": results,
-      }});
-      return true;
-  }
+  run({soundId, transcript, results}) {
+    this.unblock();
+    Sounds.collection.update(soundId, {
+      $set: {
+        'meta.transcript': transcript,
+        'meta.results': results,
+      },
+    });
+    return true;
+  },
 });
 
 export const deleteSound = new ValidatedMethod({
-  name: "sounds.delete",
+  name: 'sounds.delete',
   validate: new SimpleSchema({
-    soundId: { type: String }
+    soundId: {type: String},
   }).validator(),
-  run({ soundId }) {
+  run({soundId}) {
     try {
       Sounds.remove(soundId);
     } catch (e) {
       console.error(e);
     }
-  }
+  },
 });
 
 export const deleteTalkSounds = new ValidatedMethod({
-  name: "sounds.deleteTalkSounds",
+  name: 'sounds.deleteTalkSounds',
   validate: new SimpleSchema({
-    talkId: { type: String }
+    talkId: {type: String},
   }).validator(),
-  run({ talkId }) {
+  run({talkId}) {
     try {
-      Sounds.remove({ "meta.talkId": talkId });
+      Sounds.remove({'meta.talkId': talkId});
     } catch (e) {
       console.error(e);
     }
-  }
+  },
 });
