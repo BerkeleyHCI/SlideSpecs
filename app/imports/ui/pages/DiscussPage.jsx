3a85351d62623ee8c8dd07a99d80d660fc6852bf
Jeremy Warner <jeremywrnr@gmail.com>
2019-03-13 18:12:55 -0700

    centralize logging code

--- app/imports/ui/pages/DiscussPage.jsx
+++ app/imports/ui/pages/DiscussPage.jsx
@@ -1,33 +1,33 @@
-import { Meteor } from "meteor/meteor";
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import { Session } from "meteor/session.js";
-import { withTracker } from "meteor/react-meteor-data";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import _ from "lodash";
+import {Meteor} from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {Session} from 'meteor/session.js';
+import {withTracker} from 'meteor/react-meteor-data';
+import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import _ from 'lodash';
 
-import { Images } from "../../api/images/images.js";
-import BaseComponent from "../components/BaseComponent.jsx";
-import AlertLink from "../components/AlertLink.jsx";
-import AppNotification from "../components/AppNotification.jsx";
-import SpeechRecognition from "react-speech-recognition";
-import Input from "../components/Input.jsx";
-import TextArea from "../components/TextArea.jsx";
-import SlideTags from "../components/SlideTags.jsx";
-import ClearingDiv from "../components/ClearingDiv.jsx";
-import FileReview from "../components/FileReview.jsx";
-import Clock from "../components/Clock.jsx";
-import Img from "../components/Image.jsx";
-import Message from "../components/Message.jsx";
-import Comment from "../components/Comment.jsx";
-import { Comments } from "../../api/comments/comments.js";
-import { createComment, addressComment } from "../../api/comments/methods.js";
-import { setRespondingComment } from "../../api/talks/methods.js";
+import {Images} from '../../api/images/images.js';
+import BaseComponent from '../components/BaseComponent.jsx';
+import AlertLink from '../components/AlertLink.jsx';
+import AppNotification from '../components/AppNotification.jsx';
+import SpeechRecognition from 'react-speech-recognition';
+import Input from '../components/Input.jsx';
+import TextArea from '../components/TextArea.jsx';
+import SlideTags from '../components/SlideTags.jsx';
+import ClearingDiv from '../components/ClearingDiv.jsx';
+import FileReview from '../components/FileReview.jsx';
+import Clock from '../components/Clock.jsx';
+import Img from '../components/Image.jsx';
+import Message from '../components/Message.jsx';
+import Comment from '../components/Comment.jsx';
+import {Comments} from '../../api/comments/comments.js';
+import {createComment, addressComment} from '../../api/comments/methods.js';
+import {setRespondingComment} from '../../api/talks/methods.js';
 
 // Control-log.
-import { Logger } from "meteor/ostrio:logger";
-import { LoggerConsole } from "meteor/ostrio:loggerconsole";
+import {Logger} from 'meteor/ostrio:logger';
+import {LoggerConsole} from 'meteor/ostrio:loggerconsole';
 
 class DiscussPage extends BaseComponent {
   constructor(props) {
@@ -40,28 +40,28 @@ class DiscussPage extends BaseComponent {
     this.inRef = React.createRef();
     this.state = {
       redirectTo: null,
-      sorter: "flag",
-      filter: "flag",
+      sorter: 'flag',
+      filter: 'flag',
       invert: true,
-      bySlide: "",
-      byAuth: "",
-      byTag: "",
-      hoverImage: "",
-      image: ""
+      bySlide: '',
+      byAuth: '',
+      byTag: '',
+      hoverImage: '',
+      image: '',
     };
   }
 
   handleLoad = () => {
-    const grid = document.getElementById("grid");
+    const grid = document.getElementById('grid');
     const mason = new Masonry(grid, {
-      itemSelector: ".file-item"
+      itemSelector: '.file-item',
     });
   };
 
   extractFileData = x => {
     return {
-      slideId: x.getAttribute("data-file-id"),
-      slideNo: x.getAttribute("data-iter")
+      slideId: x.getAttribute('data-file-id'),
+      slideNo: x.getAttribute('data-iter'),
     };
   };
 
@@ -70,13 +70,12 @@ class DiscussPage extends BaseComponent {
       const data = this.extractFileData(e.target);
       const id = data.slideId;
       try {
-        const { image, hoverImage } = this.state;
-        const newhoverImage = Images.findOne(id).link("original", "//");
+        const {image, hoverImage} = this.state;
+        const newhoverImage = Images.findOne(id).link('original', '//');
         if (newhoverImage) {
-          if (newhoverImage !== image)
-            this.setState({ image: newhoverImage });
+          if (newhoverImage !== image) this.setState({image: newhoverImage});
           if (newhoverImage !== hoverImage)
-            this.setState({ hoverImage: newhoverImage });
+            this.setState({hoverImage: newhoverImage});
         }
       } catch (e) {
         console.error(e);
@@ -84,112 +83,94 @@ class DiscussPage extends BaseComponent {
     }
   };
 
-  log = data => {
-    //console.log(data);
-    const { reviewer, talk } = this.props;
-    if (typeof data === "string") {
-      this.logger.info(
-        JSON.stringify({ data, reviewer, talk, time: Date.now() })
-      );
-    } else if (Object.keys.length > 0) {
-      this.logger.info(
-        JSON.stringify({ ...data, reviewer, talk, time: Date.now() })
-      );
-    } else {
-      this.logger.info(
-        JSON.stringify({ data, reviewer, talk, time: Date.now() })
-      );
-    }
-  };
-
   componentDidMount = () => {
     // set image to link of the first slide
     this.handleLoad(); // masonry
-    const { images } = this.props;
+    const {images} = this.props;
     if (images.length > 0) {
       this.updateImage(images[0]._id);
     }
   };
 
   setByAuth = e => {
-    const { byAuth } = this.state;
-    const newAuth = e.target.getAttribute("data-auth");
+    const {byAuth} = this.state;
+    const newAuth = e.target.getAttribute('data-auth');
     if (newAuth && byAuth === newAuth) {
-      this.setState({ byAuth: "" });
+      this.setState({byAuth: ''});
     } else if (newAuth) {
-      this.setState({ byAuth: newAuth });
+      this.setState({byAuth: newAuth});
     }
   };
 
   clearByAuth = () => {
-    this.setState({ byAuth: "" });
+    this.setState({byAuth: ''});
   };
 
   setBySlide = e => {
-    const { bySlide } = this.state;
+    const {bySlide} = this.state;
     const newSlide = e.target.innerText.trim();
     if (newSlide && bySlide === newSlide) {
-      this.setState({ bySlide: "" });
+      this.setState({bySlide: ''});
     } else if (newSlide) {
-      this.setState({ bySlide: newSlide });
+      this.setState({bySlide: newSlide});
     }
   };
 
   clearBySlide = () => {
-    this.setState({ bySlide: "" });
+    this.setState({bySlide: ''});
   };
 
   // click on tag in comment
   setByTag = e => {
     e.preventDefault();
-    const { byTag } = this.state;
+    const {byTag} = this.state;
     const newTag = e.target.innerText.trim();
     if (newTag && byTag === newTag) {
-      this.setState({ byTag: "" });
+      this.setState({byTag: ''});
     } else if (newTag) {
-      this.setState({ byTag: newTag });
+      this.setState({byTag: newTag});
     }
   };
 
   clearByTag = () => {
-    this.setState({ byTag: "" });
+    this.setState({byTag: ''});
   };
 
   updateImage = fid => {
-    const link = Images.findOne({ _id: fid }).link("original", "//");
-    this.setState({ image: link });
+    const link = Images.findOne({_id: fid}).link('original', '//');
+    this.setState({image: link});
   };
 
   updateHoverImage = link => {
-    this.setState({ hoverImage: link, image: link });
+    this.setState({hoverImage: link, image: link});
   };
 
   handleSlideIn = e => {
-    const src = e.target.querySelector("img").src;
+    const src = e.target.querySelector('img').src;
     if (src) this.updateHoverImage(src);
   };
 
   handleSlideOut = e => {
-    this.setState({ hoverImage: false });
+    this.setState({hoverImage: false});
   };
 
   renderCommentFilter = () => {
     const filterer = this.renderFilter();
 
-    const { images } = this.props;
-    const { control, invert, filter } = this.state;
-    const invFn = () => this.setState({ invert: !invert });
+    const {images} = this.props;
+    const {control, invert, filter} = this.state;
+    const invFn = () => this.setState({invert: !invert});
     const setSort = (s, f) => {
-      return () => this.setState({ sorter: s, filter: f });
+      return () => this.setState({sorter: s, filter: f});
     };
 
-    const timeSort = setSort("created", "time");
-    const authSort = setSort(x => x.author.toLowerCase(), "auth");
-    const agreeSort = setSort(x => (x.agree || []).length, "agree");
-    const flagSort = setSort(x => (x.discuss || []).length, "flag");
+    const timeSort = setSort('created', 'time');
+    const authSort = setSort(x => x.author.toLowerCase(), 'auth');
+    const agreeSort = setSort(x => (x.agree || []).length, 'agree');
+    const flagSort = setSort(x => (x.discuss || []).length, 'flag');
     const slideSort = setSort(
       x => (x.slides[0] ? Number(x.slides[0].slideNo) : Infinity),
-      "slide"
+      'slide',
     );
 
     return (
@@ -197,36 +178,31 @@ class DiscussPage extends BaseComponent {
         <div className="btn-m-group btns-group">
           <button
             onClick={timeSort}
-            className={"btn btn-menu" + (filter === "time" ? " active" : "")}
-          >
+            className={'btn btn-menu' + (filter === 'time' ? ' active' : '')}>
             time
           </button>
           <button
-            className={"btn btn-menu" + (filter === "slide" ? " active" : "")}
-            onClick={slideSort}
-          >
+            className={'btn btn-menu' + (filter === 'slide' ? ' active' : '')}
+            onClick={slideSort}>
             slide
           </button>
           <button
-            className={"btn btn-menu" + (filter === "auth" ? " active" : "")}
-            onClick={authSort}
-          >
+            className={'btn btn-menu' + (filter === 'auth' ? ' active' : '')}
+            onClick={authSort}>
             auth
           </button>
           <button
-            className={"btn btn-menu" + (filter === "agree" ? " active" : "")}
-            onClick={agreeSort}
-          >
+            className={'btn btn-menu' + (filter === 'agree' ? ' active' : '')}
+            onClick={agreeSort}>
             agree
           </button>
           <button
-            className={"btn btn-menu" + (filter === "flag" ? " active" : "")}
-            onClick={flagSort}
-          >
+            className={'btn btn-menu' + (filter === 'flag' ? ' active' : '')}
+            onClick={flagSort}>
             discuss
           </button>
-          <button className={"btn btn-menu"} onClick={invFn}>
-            {invert ? "▼" : "▲"}
+          <button className={'btn btn-menu'} onClick={invFn}>
+            {invert ? '▼' : '▲'}
           </button>
         </div>
         {filterer}
@@ -235,12 +211,12 @@ class DiscussPage extends BaseComponent {
   };
 
   renderFiles = () => {
-    const { images } = this.props;
+    const {images} = this.props;
     return images.map((f, key) => {
-      let link = Images.findOne({ _id: f._id }).link("original", "//");
+      let link = Images.findOne({_id: f._id}).link('original', '//');
       return (
         <FileReview
-          key={"file-" + key}
+          key={'file-' + key}
           iter={key + 1}
           fileUrl={link}
           fileId={f._id}
@@ -254,9 +230,9 @@ class DiscussPage extends BaseComponent {
   };
 
   renderFilter = () => {
-    let { control, byAuth, bySlide, byTag } = this.state;
-    const sType = bySlide === "general" ? "scope" : "slide";
-    const { browserSupportsSpeechRecognition } = this.props;
+    let {control, byAuth, bySlide, byTag} = this.state;
+    const sType = bySlide === 'general' ? 'scope' : 'slide';
+    const {browserSupportsSpeechRecognition} = this.props;
     if (bySlide) bySlide = <kbd>{bySlide}</kbd>;
 
     return (
@@ -269,8 +245,8 @@ class DiscussPage extends BaseComponent {
   };
 
   renderCommentData = (arr, replies, c, i) => {
-    const { sessionId, comments, reviewer, setModal, clearModal } = this.props;
-    const { sorter, invert, byAuth, bySlide, byTag, control } = this.state;
+    const {sessionId, comments, reviewer, setModal, clearModal} = this.props;
+    const {sorter, invert, byAuth, bySlide, byTag, control} = this.state;
     c.last = i === arr.length - 1; // no final hr
     c.replies = replies.filter(r => r.replyTo == c._id);
     return {
@@ -292,20 +268,20 @@ class DiscussPage extends BaseComponent {
       handleSlideOut: this.handleSlideOut,
       clearButton: this.clearButton,
       clearBySlide: this.clearBySlide,
-      setBySlide: this.setBySlide
+      setBySlide: this.setBySlide,
     };
   };
 
   renderComments = () => {
-    const { sorter, invert, byAuth, bySlide, byTag, control } = this.state;
-    const { talk, comments, reviewer, setModal, clearModal } = this.props;
+    const {sorter, invert, byAuth, bySlide, byTag, control} = this.state;
+    const {talk, comments, reviewer, setModal, clearModal} = this.props;
     if (!comments || !comments.length) {
       return <div className="alert"> no comments yet</div>;
     } else {
       let csort = _.orderBy(
         comments,
-        [sorter, "created"],
-        [invert ? "desc" : "asc", "asc"]
+        [sorter, 'created'],
+        [invert ? 'desc' : 'asc', 'asc'],
       );
 
       // Clean - filter out those without discuss.
@@ -336,7 +312,7 @@ class DiscussPage extends BaseComponent {
 
       if (bySlide) {
         csort = csort.filter(c => {
-          const general = [{ slideNo: "general" }];
+          const general = [{slideNo: 'general'}];
           const slides = c.slides.length > 0 ? c.slides : general;
           const slideNos = slides.map(x => x.slideNo);
           return slideNos.includes(bySlide);
@@ -348,11 +324,11 @@ class DiscussPage extends BaseComponent {
       }
 
       const items = csort.map((c, i) =>
-        this.renderCommentData(csort, replies, c, i)
+        this.renderCommentData(csort, replies, c, i),
       );
 
       const addressedItems = addressed.map((c, i) =>
-        this.renderCommentData(addressed, replies, c, i)
+        this.renderCommentData(addressed, replies, c, i),
       );
 
       return (
@@ -385,8 +361,8 @@ class DiscussPage extends BaseComponent {
 
   renderContext = () => {
     const fileList = this.renderFiles();
-    const { talk } = this.props;
-    const { image, hoverImage, bySlide } = this.state;
+    const {talk} = this.props;
+    const {image, hoverImage, bySlide} = this.state;
     const imgSrc = hoverImage ? hoverImage : image;
 
     return (
@@ -398,7 +374,7 @@ class DiscussPage extends BaseComponent {
         <div id="v-pad" />
         <AlertLink
           center={true}
-          text={"return to commenting"}
+          text={'return to commenting'}
           link={`/comment/${talk._id}`}
         />
       </div>
@@ -406,12 +382,12 @@ class DiscussPage extends BaseComponent {
   };
 
   clearRespond = () => {
-    const { talk } = this.props;
-    setRespondingComment.call({ talk: talk._id, commentId: "" });
+    const {talk} = this.props;
+    setRespondingComment.call({talk: talk._id, commentId: ''});
   };
 
   renderRespond = () => {
-    const { talk } = this.props;
+    const {talk} = this.props;
     if (!talk.active) return;
     const respond = Comments.findOne(talk.active);
     if (!respond) return;
@@ -426,7 +402,7 @@ class DiscussPage extends BaseComponent {
   };
 
   render() {
-    const { images } = this.props;
+    const {images} = this.props;
     const context = this.renderContext();
     const cmtHead = this.renderCommentFilter();
     const respond = this.renderRespond();
