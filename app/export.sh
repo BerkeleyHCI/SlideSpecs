1595125de4cd4a14e0f228ce147202ab13c64d15
Jeremy Warner <jeremywrnr@gmail.com>
2019-03-15 15:30:02 -0700

    adding files to export script

--- app/export.sh
+++ app/export.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-# TODO - also export files/images (non-hardcoded path way)
+NOW=$(date)
 
 # Mongo Collections to export.
-declare -a arr=("users" "Sessions" "Talks" "Comments" "files" "images")
-
-data="../data $(date)"
-mkdir -p "$data"
+declare -a arr=("users" "Talks" "Comments" "Events" "files" "images" "sounds")
+data="../db-data $NOW"
+mkdir -pv "$data"
 for i in "${arr[@]}"; do
     mongoexport --port 3001 --db meteor --collection "$i" --out "$data/$i.json"
 done
+
+# copy files/images (hardcoded path)
+files="/$HOME/Downloads/research"
+dfile="../db-files $NOW"
+cp -Rv "$files" "$dfiles"
+
+# export and zip
+dump="../db-dump-$NOW.tar.gz"
+tar -czf "$dump" "$data" "$dfiles"
+echo "Export done."
+
