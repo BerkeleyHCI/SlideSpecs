facf2c3f0be5b3476280a65fc8037afa40c8dfbb
Jeremy Warner <jeremywrnr@gmail.com>
2019-04-17 01:09:45 -0700

    secure // import code

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -143,7 +143,7 @@ export default class App extends BaseComponent {
   };
 
   renderContent = ({location, ...other}) => {
-    //this.renderSecure(); // http -> https
+    this.renderSecure(); // http -> https
     const {user, sessions, files, loading} = this.props;
     const params = queryString.parse(location.search);
     const shared = this.getSharedProps();
