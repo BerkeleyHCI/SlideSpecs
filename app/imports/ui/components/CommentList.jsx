141fd0563325600e5859c4169eaa4a5a8188b237
tonyanguyen <tonyanguyen@berkeley.edu>
2019-04-29 15:07:22 -0700

    Update export option for classroom

---
+++ app/imports/ui/components/CommentList.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment.jsx';
+
+class CommentList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: this.props.defaultOpen};
+  }
+
+  toggleOpen = () => {
+    const {open} = this.state;
+    this.setState({open: !open});
+  };
+
+  render() {
+    const {open} = this.state;
+    const {title, items, note, content, ...other} = this.props;
+    if (items.length == 0 && !content) return null;
+
+    let expander = open ? '−' : '+';
+    return (
+      <div className="comments-list alert">
+        <span
+          className={`list-title ${note ? 'list-title-note' : ''}`}
+          onClick={this.toggleOpen}>
+          {title}
+          <span className="pull-right danger">{expander}</span>
+        </span>
+        {open && content}
+        {open &&
+          items.map((item, i) => (
+            <Comment
+              key={i}
+              {...item}
+              {...other}
+              last={
+                i + 1 == items.length &&
+                (!item.replies || item.replies.length === 0)
+              }
+            />
+          ))}
+      </div>
+    );
+  }
+}
+
+CommentList.propTypes = {
+  title: PropTypes.string,
+  items: PropTypes.array,
+};
+
+CommentList.defaultProps = {
+  defaultOpen: true,
+  title: 'link',
+  items: [],
+};
+
+export default CommentList;
--- app/imports/ui/pages/TalkPage.jsx
+++ app/imports/ui/pages/TalkPage.jsx
@@ -4,11 +4,15 @@ import {Link} from 'react-router-dom';
 import {deleteTalk} from '../../api/talks/methods.js';
 import {Files} from '../../api/files/files.js';
 import {Images} from '../../api/images/images.js';
-
+import {toast} from 'react-toastify';
 import AlertLink from '../components/AlertLink.jsx';
 import BaseComponent from '../components/BaseComponent.jsx';
 import SlideFile from '../components/SlideFile.jsx';
 import TalkListItem from '../components/TalkListItem.jsx';
+import CommentList from '../components/CommentList.jsx';
+import _ from 'lodash';
+import ReactDOMServer from 'react-dom/server';
+import AppNotification from '../components/AppNotification.jsx';
 
 class TalkPage extends BaseComponent {
   updateMason = () => {
@@ -38,6 +42,182 @@ class TalkPage extends BaseComponent {
     this.redirectTo(session._id);
     deleteTalk.call({talkId});
   };
+
+
+  filterComment = c => {
+    let newComment = _.pick(c, [
+      'author',
+      'content',
+      'created',
+      'agree',
+      'discuss',
+      'replies',
+    ]);
+    c.replies = c.replies || [];
+    newComment.replies = c.replies.map(this.filterComment);
+    return newComment;
+  };
+
+  downloadJSON = () => {
+    const {comments, talk} = this.props;
+    // Filtering out 'reply' comments.
+    const reply = /\[.*\]\(\s?#c(.*?)\)/;
+    const notReply = c => !reply.test(c.content);
+    const isReply = c => reply.test(c.content);
+
+    // Add label replies with which referring comment
+    const replies = comments.filter(isReply).map(c => {
+      const match = reply.exec(c.content);
+      c.replyTo = match[1].trim();
+      c.isReply = true;
+      return c;
+    });
+
+    comments.map(c => {
+      c.replies = replies.filter(r => r.replyTo == c._id);
+    });
+
+    const filtered = comments.filter(notReply).map(this.filterComment);
+
+    const fname = `${talk.name}_comments.json`;
+    const content = JSON.stringify(filtered, null, 2);
+    this.createDownload({fname, content, type: 'application/json'});
+  };
+
+  createDownload = ({fname, content, type}) => {
+    const file = new File([content], fname, {type: type});
+    const element = document.createElement('a');
+    element.href = URL.createObjectURL(file);
+    element.download = fname;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+    toast(() => (
+      <AppNotification msg={'downloaded'} desc={fname} icon={'floppy-o'} />
+    ));
+  };
+
+  // dev-html download start
+
+  renderComments = () => {
+    const {
+      sorter,
+      invert,
+      filtered,
+      activeComment,
+      focusing,
+      userOwn,
+      byAuth,
+      bySlide,
+      byTag,
+    } = this.state;
+    const {talk, comments, reviewer, setModal, clearModal} = this.props;
+    if (!comments || !comments.length) {
+      return <div className="alert"> no comments yet</div>;
+    } else {
+      let csort = _.orderBy(
+        comments,
+        [sorter, 'created'],
+        [invert ? 'desc' : 'asc', 'asc'],
+      );
+
+      // Filter out transcript comments.
+      csort = csort.filter(c => c.author != 'transcript');
+
+      // Focus view filtering - omit replies.
+      if (userOwn) {
+        csort = csort.filter(c => c.author === reviewer);
+      }
+
+      // Filtering 'reply' comments into array.
+      // TODO - make it so this seperates on punctuation
+      const reply = /\[.*\]\(\s?#c(.*?)\)/;
+      const isReply = c => reply.test(c.content);
+      const replies = csort.filter(isReply).map(c => {
+        const match = reply.exec(c.content);
+        c.replyTo = match[1].trim();
+        c.isReply = true;
+        return c;
+      });
+
+      // remove child comments.
+      csort = csort.filter(c => !isReply(c));
+
+      if (byAuth) {
+        csort = csort.filter(c => c.author === byAuth);
+      }
+
+      if (bySlide) {
+        csort = csort.filter(c => {
+          const slides = c.slides.length > 0 ? c.slides : [];
+          const slideNos = slides.map(x => x.slideNo);
+          return slideNos.includes(bySlide);
+        });
+      }
+
+      if (byTag) {
+        csort = csort.filter(c => c.content.includes(byTag));
+      }
+
+      const items = csort.map((c, i) => {
+        c.last = i === csort.length - 1; // no final hr
+        c.active = c._id === activeComment; // highlight
+        c.replies = replies.filter(r => r.replyTo == c._id);
+        return {
+          ...c,
+          key: c._id,
+          commentView: true,
+          focused: true,
+          allReplies: replies,
+        };
+      });
+
+      const homeLink = window.location.origin + '/review/' + talk._id;
+      return (
+        <div>
+          <AlertLink
+            text={'Open the talk page on SlideSpecs with this link.'}
+            link={homeLink}
+          />
+          <span className="comments-head" />
+          <CommentList title={'comments'} items={items} />
+          {items.length == 0 && <div className="alert"> no comments</div>}
+        </div>
+      );
+    }
+  };
+
+  downloadHTML = () => {
+    const renderComments = this.renderComments();
+    const styleURL = document.querySelectorAll('link.__meteor-css__')[0].href;
+    const commentStyle = this.httpGet(styleURL);
+    const commentHtml = ReactDOMServer.renderToString(renderComments);
+    const documentBody = `
+    <head>
+    <meta charset="utf-8"/>
+    <style>${commentStyle}</style>
+    </head>
+    <body>
+      <div class="padded">
+        ${commentHtml}
+      </div>
+    </body>`;
+    const {talk} = this.props;
+    const fname = `${talk.name}_comments.html`;
+    const content = documentBody;
+    this.createDownload({fname, content, type: 'text/html'});
+  };
+
+  httpGet = theUrl => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+  };
+
+  // dev-html download end
+
+
 
   render() {
     const {session, talk, name, images, comments, sessionOwner} = this.props;
@@ -82,13 +262,8 @@ class TalkPage extends BaseComponent {
             text={'view commenting interface'}
             link={commentLink}
           />
-          <AlertLink
-            center={true}
-            text={'download comments'}
-            link={downloadLink}
-          />
 
-          <div className="alert">
+          <div className="clearfix alert">
             <ul>
               <li>slides: {images.length}</li>
               <li>comments: {comments.length}</li>
@@ -96,11 +271,19 @@ class TalkPage extends BaseComponent {
             <hr />
 
             <div className="btns-menu-space">
+              <button onClick={this.downloadJSON} className="btn btn-menu btn-primary">
+                  download json
+              </button>
+                <button onClick={this.downloadHTML} className="btn btn-menu btn-primary">
+                  download html
+                </button>
               <a download href={talkFile}>
                 <button className="btn btn-menu btn-primary pull-right">
                   download slides pdf
                 </button>
               </a>
+
+
             </div>
           </div>
 
