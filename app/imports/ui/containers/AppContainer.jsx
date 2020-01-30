6a1c9135f7929e9219b41d6318f532e7d53f48d6
Jeremy Warner <jeremywrnr@gmail.com>
2020-01-30 15:30:16 -0800

    prettier

--- app/imports/ui/containers/AppContainer.jsx
+++ app/imports/ui/containers/AppContainer.jsx
@@ -1,20 +1,20 @@
-import {Meteor} from 'meteor/meteor';
-import {Session} from 'meteor/session.js';
-import {withTracker} from 'meteor/react-meteor-data';
+import { Meteor } from "meteor/meteor";
+import { Session } from "meteor/session.js";
+import { withTracker } from "meteor/react-meteor-data";
 
-import {Talks} from '../../api/talks/talks.js';
-import {Comments} from '../../api/comments/comments.js';
-import {Files} from '../../api/files/files.js';
-import {Images} from '../../api/images/images.js';
-import {Sounds} from '../../api/sounds/sounds.js';
-import {Transcripts} from '../../api/transcripts/transcripts.js';
+import { Talks } from "../../api/talks/talks.js";
+import { Comments } from "../../api/comments/comments.js";
+import { Files } from "../../api/files/files.js";
+import { Images } from "../../api/images/images.js";
+import { Sounds } from "../../api/sounds/sounds.js";
+import { Transcripts } from "../../api/transcripts/transcripts.js";
 
-import App from '../layouts/App.jsx';
-import 'react-toastify/dist/ReactToastify.min.css';
+import App from "../layouts/App.jsx";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default withTracker(() => {
-  const sub = Session.get('subscription');
-  const reviewer = Session.get('reviewer');
+  const sub = Session.get("subscription");
+  const reviewer = Session.get("reviewer");
   let data = {
     connected: Meteor.status().connected,
     user: Meteor.user(),
@@ -28,7 +28,7 @@ export default withTracker(() => {
 
     // Session data.
     reviewer,
-    sub,
+    sub
   };
 
   if (sub) {
@@ -40,12 +40,12 @@ export default withTracker(() => {
     const transcripts = Meteor.subscribe(`transcripts.${sub.type}`, sub._id);
     data = Object.assign(data, {
       loading: [talks, comments, files, images].some(s => !s.ready()),
-      talks: Talks.find({}, {sort: {created: -1}}).fetch(),
-      comments: Comments.find({}, {sort: {name: 1}}).fetch(),
-      files: Files.find({}, {sort: {name: 1}}).fetch(),
-      images: Images.find({}, {sort: {name: 1}}).fetch(),
-      sounds: Sounds.find({}, {sort: {'meta.created': -1}}).fetch(),
-      transcripts: Transcripts.find({}, {sort: {created: -1}}).fetch(),
+      talks: Talks.find({}, { sort: { created: -1 } }).fetch(),
+      comments: Comments.find({}, { sort: { name: 1 } }).fetch(),
+      files: Files.find({}, { sort: { name: 1 } }).fetch(),
+      images: Images.find({}, { sort: { name: 1 } }).fetch(),
+      sounds: Sounds.find({}, { sort: { "meta.created": -1 } }).fetch(),
+      transcripts: Transcripts.find({}, { sort: { created: -1 } }).fetch()
     });
   }
 
