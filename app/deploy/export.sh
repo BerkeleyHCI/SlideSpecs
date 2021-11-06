894f49824948d82f4a25732765680c195ac21d1f
Jeremy Warner <jeremywrnr@gmail.com>
2021-11-06 01:48:48 -0700

    exporting options

---
+++ app/deploy/export.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# run in / git.root
+
+NOW=$(date)
+
+# Mongo Collections to export.
+declare -a arr=("users" "Talks" "Comments" "Events" "Transcripts" "files" "images" "sounds")
+data="db-data $NOW"
+mkdir -pv "$data"
+for i in "${arr[@]}"; do
+    echo "Processing $i"
+    mongoexport --port 3001 --db meteor --collection "$i" --out "$data/$i.json"
+done
+
+# copy files/images (hardcoded path)
+if [[ $1 != "--skip-files" ]]; then
+    files="$HOME/Downloads/research"
+    dfile="db-files $NOW"
+    mkdir -pv "$dfile"
+    cp -av "$files/." "$dfile/"
+fi
+
+# export and zip
+echo "Compressing."
+dump="data.tar.gz"
+if [[ -n $dfile ]]; then
+    tar -czf "$dump" "$data" "$dfile"
+else
+    tar -czf "$dump" "$data"
+fi
+echo "Export done."
+
---
+++ app/deploy/import.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# provide one arg to specify which folder json is in.
+# https://markdrew.io/importing-json-into-a-meteor-mongo-database
+
+for i in `find "$1" -name '*.json'`; do
+    echo importing $i
+    full=$(basename -- "$i")
+    base="${full%.*}"
+    mongoimport -h localhost:3001 --db meteor \
+        --collection "$base" \
+        --type json \
+        --file "$i"
+done
+
