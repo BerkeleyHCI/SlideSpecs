a4c5a5545f3f13945ad346c7c0e46afe2f0232c9
Jeremy Warner <jeremywrnr@gmail.com>
2021-11-11 15:10:12 -0800

    slidespecs root

--- app/imports/api/storagePath.js
+++ app/imports/api/storagePath.js
@@ -5,4 +5,4 @@ export const bucketName = Meteor.isProduction ? "slidespecs" : "slidespecs-dev";
 
 // Place for saving files locally.
 //export const storagePath = '/Users/tonyanguyen/Downloads/research';
-export const storagePath = "/Users/jeremy/Documents/slidespecs-db";
+export const storagePath = "/Users/jwrnr/Documents/slidespecs-db";
